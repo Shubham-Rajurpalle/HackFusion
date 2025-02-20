c6fcf484d0f769a332612b5a93cb30a238829843
Shubham-Rajurpalle <rajurpalleshubham1802@gmail.com>
2025-02-20 16:34:07 +0530

    Updated Structure

--- app/src/main/java/com/hackfusion/MainActivity.kt
+++ app/src/main/java/com/hackfusion/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.cricketapp.hackfusion
+package com.hackfusion
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.cricketapp.hackfusion.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/hackfusion/complaint_faculty_fragment.kt
+++ app/src/main/java/com/hackfusion/complaint_faculty_fragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.cricketapp.hackfusion.R
 
-class Facility_Fragment : Fragment() {
+class complaint_faculty_fragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +17,6 @@ class Facility_Fragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_facility_, container, false)
+        return inflater.inflate(R.layout.fragment_complaint_faculty, container, false)
     }
 }
